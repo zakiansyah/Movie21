2cd3db3d71e782a1549494cdb06a279597765ab6
zakiansyah4848 <ansyahzaki69@gmail.com>
2024-02-12 21:50:04 +0700

    update dependency

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -54,6 +54,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.annotation:annotation:1.7.1")
+
 
     implementation ("androidx.core:core-splashscreen:1.0.1")
 
